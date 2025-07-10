7084ec949f55d67a27121d22f1f17041f174f63b
izzatillo-swe <izzatillotilavoldiyev5@gmail.com>
2025-07-10 12:54:15 +0500

    kotlin with spring boot project committed

---
+++ src/main/kotlin/com/company/kotlin_spring_boot/KotlinSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.company.kotlin_spring_boot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringBootApplication>(*args)
+}
---
+++ src/test/kotlin/com/company/kotlin_spring_boot/KotlinSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.company.kotlin_spring_boot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
