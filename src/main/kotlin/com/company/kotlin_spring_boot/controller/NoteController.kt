d46d7ad976de03a628689622bd602e901d817485
izzatillo-swe <izzatillotilavoldiyev5@gmail.com>
2025-07-10 14:32:50 +0500

    Note entity, controller, repository and create and get apis added

---
+++ src/main/kotlin/com/company/kotlin_spring_boot/controller/NoteController.kt
@@ -0,0 +1,73 @@
+package com.company.kotlin_spring_boot.controller
+
+import com.company.kotlin_spring_boot.model.Note
+import com.company.kotlin_spring_boot.repository.NoteRepository
+import org.bson.types.ObjectId
+import org.springframework.web.bind.annotation.*
+import java.time.Instant
+
+@RestController
+@RequestMapping("/notes")
+class NoteController(
+    private val repository: NoteRepository,
+) {
+
+    data class NoteRequest(
+        val id: String?,
+        val title: String,
+        val content: String,
+        val color: Long
+    )
+
+    data class NoteResponse(
+        val id: String,
+        val title: String,
+        val content: String,
+        val color: Long,
+        val createdAt: Instant,
+    )
+
+    @PostMapping
+    fun save(
+        @RequestBody body: NoteRequest
+    ): NoteResponse {
+        val note = repository.save(
+            Note(
+                id = body.id?.let { ObjectId(it) } ?: ObjectId.get(),
+                title = body.title,
+                content = body.content,
+                color = body.color,
+                createdAt = Instant.now(),
+                ownerId = ObjectId()
+            )
+        )
+
+        return NoteResponse(
+            id = note.id.toHexString(),
+            title = note.title,
+            content = note.content,
+            color = note.color,
+            createdAt = note.createdAt
+        )
+    }
+
+    @GetMapping
+    fun findByOwnerId(
+        @RequestParam(required = true) ownerId: String,
+    ): List<NoteResponse> {
+        return repository.findByOwnerId(ObjectId(ownerId)).map {
+            it.toResponse()
+        }
+    }
+
+}
+
+private fun Note.toResponse(): NoteController.NoteResponse {
+    return NoteController.NoteResponse(
+        id = id.toHexString(),
+        title = title,
+        content = content,
+        color = color,
+        createdAt = Instant.now(),
+    )
+}
---
+++ src/main/kotlin/com/company/kotlin_spring_boot/repository/NoteRepository.kt
@@ -0,0 +1,9 @@
+package com.company.kotlin_spring_boot.repository
+
+import com.company.kotlin_spring_boot.model.Note
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface NoteRepository: MongoRepository<Note, ObjectId> {
+    fun findByOwnerId(ownerId: ObjectId): List<Note>
+}
